5c772f6d998070cb87ebdf29ca7de7b49e4b487b
liuzhu <1014166283@qq.com>
2018-12-09 14:31:42 +0800

    Add local strage.

--- Todo/main.js
+++ Todo/main.js
@@ -7,7 +7,6 @@ class TodoList extends React.Component {
     render() {
         return (
             <li>
-                <span className="serialNum">{this.props.index + 1}</span>
                 <span className="recordValue"> {this.props.name}</span>
 
             </li>
@@ -22,11 +21,15 @@ class TodoTable extends React.Component {
 
     render() {
         let rows = [];
-        this.props.recordset.forEach((record, index) => {
-            rows.push(<TodoList name={record.name} index={index} key={index.toString()}/>)
-        });
+        let recordset = this.props.recordset;
+        if(recordset) {
+            recordset.forEach((record, index) => {
+                rows.push(<TodoList name={record.name}  key={index.toString()}/>)
+            });
+        }
+
         return (
-            <ul>{rows}</ul>
+            <ol>{rows}</ol>
         )
     }
 }
@@ -42,21 +45,27 @@ class InputBar extends React.Component {
         this.props.onRecordTextInput(e.target.value)
     }
 
-    handleRecordTextAddChange() {
-        this.props.onRecordTextAdd()
+    handleRecordTextAddChange(e) {
+        this.props.onRecordTextAdd();
+        e.preventDefault();
     }
     render() {
         return (
-            <div>
-                {/*<input type="checkbox"/>*/}
-                <input
-                    type="text"
-                    value={this.props.recordText}
-                    onChange={this.handleRecordTextInputChange}
-                />
-                <button
-                    onClick={this.handleRecordTextAddChange}
-                >Add</button>
+            <div className="SearchBar">
+                <form >
+                    <div>
+                        <input
+                            type="text"
+                            value={this.props.recordText}
+                            onChange={this.handleRecordTextInputChange}
+                        />
+                    </div>
+                    <div >
+                        <button
+                            onClick={this.handleRecordTextAddChange}
+                        >Add</button>
+                    </div>
+                </form>
             </div>
         )
     }
@@ -69,7 +78,7 @@ class RecordTodoTable extends React.Component {
         super(props);
         this.state = {
             recordText: '',
-            records: [
+            recordset: [
                 {name: '看书'},
                 {name: '听歌'},
                 {name: '煮饭'},
@@ -77,33 +86,54 @@ class RecordTodoTable extends React.Component {
         };
         this.handleRecordTextInput = this.handleRecordTextInput.bind(this);
         this.handleRecordTextAdd = this.handleRecordTextAdd.bind(this);
+        this.handleLocalStorage = this.handleLocalStorage.bind(this);
     }
 
     handleRecordTextInput(recordText) {
         this.setState({recordText: recordText})
     }
     handleRecordTextAdd() {
-        let newRecordset = this.state.records.concat();
+        let storage = getLocalStorage();
+        let newRecordset = this.state.recordset.concat();
         newRecordset.unshift({name: this.state.recordText});
-        this.setState({records: newRecordset});
+        this.setState({recordset: newRecordset, recordText: ''});
+        storage.setItem('recordset', JSON.stringify(newRecordset));
+    }
+    handleLocalStorage() {
+        let storage = getLocalStorage();
+        if(typeof storage.recordset === 'undefined') {
+            storage.setItem('recordset', JSON.stringify([]));
+        }
+        this.setState({recordset: JSON.parse(storage.recordset)});
     }
 
+    componentWillMount() {
+        this.handleLocalStorage()
+    }
     render() {
         return (
-            <div>
+            <div className="container">
                 <InputBar
                     recordText={this.state.recordText}
                     onRecordTextInput={this.handleRecordTextInput}
                     onRecordTextAdd={this.handleRecordTextAdd}
                 />
                 <TodoTable
-                    recordset={this.state.records}
+                    recordset={this.state.recordset}
                 />
             </div>
         )
     }
 }
 
+
+function getLocalStorage() {
+    if(typeof localStorage === 'object') {
+        return localStorage;
+    }else {
+        throw new Error('local storage 无法使用！')
+    }
+}
 
 ReactDOM.render(
   <RecordTodoTable/>,
